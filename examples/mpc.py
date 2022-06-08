75e351b4b6aa2aad2d641f48bcc7e4ad447d4de2
Timur Akhtyamov <rentgeny05@gmail.com>
2022-06-08 20:16:48 +0300

    Working example on do-mpc collision avoidance

--- examples/mpc.py
+++ examples/mpc.py
@@ -21,11 +21,13 @@ from pyminisim.visual import Renderer, CircleDrawing
 
 class DoMPCController:
 
-    def __init__(self, dt: float, horizon: int = 30):
+    def __init__(self, dt: float, goal: np.ndarray, sim: Simulation,
+                 obstacles: np.ndarray, horizon: int = 80):
         self._dt = dt
+        self._sim = sim
         self._horizon = horizon
 
-        self._goal = np.array([0., 0., 0.])
+        self._goal = goal.copy()
 
         self._model = do_mpc.model.Model("discrete")
 
@@ -36,20 +38,40 @@ class DoMPCController:
         u_v = self._model.set_variable(var_type='_u', var_name='u_v')
         u_omega = self._model.set_variable(var_type='_u', var_name='u_omega')
 
+        goal_x = self._model.set_variable('_p', 'goal_x')
+        goal_y = self._model.set_variable('_p', 'goal_y')
+        goal_theta = self._model.set_variable('_p', 'goal_theta')
+        # obstacle_distance = self._model.set_variable(var_type='_tvp', var_name='obstacle_distance')
+
         self._model.set_rhs('pose_x', pose_x + u_v * casadi.cos(pose_theta) * self._dt)
         self._model.set_rhs('pose_y', pose_y + u_v * casadi.sin(pose_theta) * self._dt)
         self._model.set_rhs('pose_theta', pose_theta + u_omega * self._dt)
 
-        self._model.set_expression(expr_name='cost', expr=casadi.sqrt((pose_x - self._goal[0]) ** 2 +
-                                                                      (pose_y - self._goal[1]) ** 2 +
-                                                                      (pose_theta - self._goal[2]) ** 2))
+        # obstacle_distances = []
+        # for i in range(obstacles.shape[0]):
+        #     obstacle = obstacles[i]
+        #     dist = casadi.sqrt((pose_x - obstacle[0]) ** 2 + (pose_y - obstacle[1]) ** 2) - obstacle[2]
+        #     obstacle_distances.append(dist)
+        # obstacle_distances = casadi.fmax(0.3 - (casadi.sqrt((pose_x - obstacles[0, 0]) ** 2 + (pose_y - obstacles[0, 1]) ** 2) - obstacles[0, 2] - 0.35), 0.)
+        obstacle_distance = (casadi.sqrt((pose_x - obstacles[0, 0]) ** 2 + (pose_y - obstacles[0, 1]) ** 2) - obstacles[0, 2] - 0.35)
+        self._model.set_expression("obstacle_distance", obstacle_distance)
+
+        # print((np.sqrt((sim.current_state.world.robot.pose[0] - obstacles[0, 0]) ** 2 + (sim.current_state.world.robot.pose[1] - obstacles[0, 1]) ** 2)) - obstacles[0, 2] - 0.35)
+        # print(np.linalg.norm(sim.current_state.world.robot.pose[:2] - obstacles[0, :2]))
+
+        self._model.set_expression(expr_name='cost',
+                                   expr=casadi.sqrt((pose_x - goal[0]) ** 2 +
+                                                    (pose_y - goal[1]) ** 2 +
+                                                    (pose_theta - goal[2]) ** 2) ** 2)
+                                        # casadi.mmin([(pose_x)]))
+                                        # obstacle_distance ** 3)
 
         self._model.setup()
 
         self._mpc = do_mpc.controller.MPC(self._model)
         setup_mpc = {
             'n_robust': 0,
-            'n_horizon': 30,
+            'n_horizon': horizon,
             't_step': 0.1,
             'state_discretization': 'discrete',
             'store_full_solution': True,
@@ -57,6 +79,10 @@ class DoMPCController:
             # 'nlpsol_opts': {'ipopt.linear_solver': 'MA27'}
         }
         self._mpc.set_param(**setup_mpc)
+
+        # controller_tvp_template = self._mpc.get_tvp_template()
+        # self._mpc.set_tvp_fun(lambda t_now, template=controller_tvp_template:
+        #                       self._tvp_fun_controller(template, t_now))
 
         mterm = self._model.aux['cost']  # terminal cost
         lterm = self._model.aux['cost']  # terminal cost
@@ -66,20 +92,31 @@ class DoMPCController:
 
         self._mpc.bounds['lower', '_x', 'pose_theta'] = -np.pi
         self._mpc.bounds['upper', '_x', 'pose_theta'] = np.pi
-        self._mpc.bounds['lower', '_u', 'u_v'] = -0.5
-        self._mpc.bounds['upper', '_u', 'u_v'] = 1.0
-        self._mpc.bounds['lower', '_u', 'u_omega'] = -np.deg2rad(25.0)
-        self._mpc.bounds['upper', '_u', 'u_omega'] = np.deg2rad(25.0)
+        self._mpc.bounds['lower', '_u', 'u_v'] = 0.
+        self._mpc.bounds['upper', '_u', 'u_v'] = 1.8
+        self._mpc.bounds['lower', '_u', 'u_omega'] = -np.deg2rad(30.)
+        self._mpc.bounds['upper', '_u', 'u_omega'] = np.deg2rad(30.)
 
         # self._mpc.set_rterm(u=1e-4)  # input penalty
 
+        self._mpc.set_uncertainty_values(goal_x=np.array([goal[0]]),
+                                         goal_y=np.array([goal[1]]),
+                                         goal_theta=np.array([goal[2]]))
+
+        # self._mpc.set_nl_cons("obstacle_distances", obstacle_distances, 0)
+        self._mpc.set_nl_cons('obstacles', -self._model.aux['obstacle_distance'], 0)
+
         self._mpc.setup()
 
-        self._estimator = do_mpc.estimator.StateFeedback(self._model)
+        self._mpc_estimator = do_mpc.estimator.StateFeedback(self._model)
 
-        self._simulator = do_mpc.simulator.Simulator(self._model)
-        self._simulator.set_param(t_step=0.01)
-        self._simulator.setup()
+        self._mpc_simulator = do_mpc.simulator.Simulator(self._model)
+        self._mpc_simulator.set_param(t_step=0.01)
+        p_num_simulator = self._mpc_simulator.get_p_template()
+        self._mpc_simulator.set_p_fun(lambda t_now, goal=goal, p_template=p_num_simulator: self._p_fun_simulator(goal, p_template, t_now))
+        # simulator_tvp_template = self._mpc_simulator.get_tvp_template()
+        # self._mpc_simulator.set_tvp_fun(lambda t_now, template=simulator_tvp_template: self._tvp_fun_simulator(template, t_now))
+        self._mpc_simulator.setup()
 
     @property
     def dt(self) -> float:
@@ -89,26 +126,34 @@ class DoMPCController:
     def goal(self) -> np.ndarray:
         return self._goal.copy()
 
-    @goal.setter
-    def goal(self, value: np.ndarray):
-        self._goal = value.copy()
+    def _p_fun_simulator(self, goal, p_template, t_now):
+        p_template["goal_x"] = goal[0]
+        p_template["goal_y"] = goal[1]
+        p_template["goal_theta"] = goal[2]
+        return p_template
 
-    def _get_A_matrix(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
-        return np.eye(3)
+    def _tvp_fun_controller(self, tvp_template, t_now):
+        x = self._mpc.x0
+        dist = 0.3 - casadi.sqrt((x[0] - 3.) ** 2 + (x[1] - 3.) ** 2) - 0.5
+        if dist < 0.:
+            dist = 0.
+        if t_now != 0:
+            print(t_now)
+        for i in range(self._horizon + 1):
+            tvp_template["_tvp", i, "obstacle_distance"] = dist
+        return tvp_template
 
-    def _get_B_matrix(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
-        B = np.array([[casadi.cos(x[2]) * self._dt, 0.],
-                      [casadi.sin(x[2]) * self._dt, 0.],
-                      [0., self._dt]])
-        return B
-
-    def _step_cost(self, x: np.ndarray, u: np.ndarray) -> float:
-        return np.linalg.norm(x[:2] - self._goal[:2]) + np.linalg.norm(u)
+    def _tvp_fun_simulator(self, tvp_template, t_now):
+        tvp_template["obstacle_distance"] = 0.
+        return tvp_template
 
     def predict(self, x_current: np.ndarray) -> np.ndarray:
+        self._mpc.set_uncertainty_values(goal_x=np.array([self._goal[0]]),
+                                         goal_y=np.array([self._goal[1]]),
+                                         goal_theta=np.array([self._goal[2]]))
         self._mpc.x0 = x_current
-        self._simulator.x0 = x_current
-        self._estimator.x0 = x_current
+        self._mpc_simulator.x0 = x_current
+        self._mpc_estimator.x0 = x_current
         # Use initial state to set the initial guess.
         self._mpc.set_initial_guess()
 
@@ -119,7 +164,6 @@ class DoMPCController:
 
 
 class MPCController:
-
     _STATE_DIM = 3
     _CONTROL_DIM = 2
 
@@ -183,13 +227,17 @@ class MPCController:
     def _wrap_angle(theta: float) -> float:
         return (theta + np.pi) % (2 * np.pi) - np.pi
 
+
+OBSTACLES = np.array([[3, 3, 0.8]])
+
+
 def create_sim() -> Tuple[Simulation, Renderer]:
-    robot_model = UnicycleRobotModel(initial_pose=np.array([2.0, 3.85, 0.0]),
+    robot_model = UnicycleRobotModel(initial_pose=np.array([1., 3.85, 0.0]),
                                      initial_control=np.array([0.0, np.deg2rad(0.0)]))
     sensor_noise = None
     sensors = [PedestrianDetector(noise=sensor_noise), OmniObstacleDetector()]
     sim = Simulation(sim_dt=0.01,
-                     world_map=EmptyWorld(), # CirclesWorld(circles=np.array([[3, 3, 1]])),
+                     world_map=CirclesWorld(circles=OBSTACLES),
                      robot_model=robot_model,
                      pedestrians_model=None,
                      sensors=sensors)
@@ -203,8 +251,7 @@ def main():
     sim, renderer = create_sim()
     renderer.initialize()
 
-    controller = DoMPCController(dt=0.1)
-    controller.goal = np.array([1., 1., 0.])
+    controller = DoMPCController(dt=0.1, goal=np.array([4., 1., 0.]), sim=sim, obstacles=OBSTACLES)
     renderer.draw("goal", CircleDrawing(controller.goal[:2], 0.1, (255, 0, 0), 0))
 
     running = True
@@ -223,6 +270,8 @@ def main():
 
         sim.step(u_pred)
         hold_time += sim.sim_dt
+        pose = sim.current_state.world.robot.pose
+        print(pose)
 
     # Done! Time to quit.
     pygame.quit()
