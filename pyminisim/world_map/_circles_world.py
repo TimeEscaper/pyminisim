af6b2bca44ddc7262b00fce824d4099333fd0752
Timur Akhtyamov <rentgeny05@gmail.com>
2022-06-14 11:33:05 +0300

    Radius param added to closest distance map interface

--- pyminisim/world_map/_circles_world.py
+++ pyminisim/world_map/_circles_world.py
@@ -15,15 +15,14 @@ class CirclesWorld(AbstractWorldMap):
         super(CirclesWorld, self).__init__()
         self._circles = circles.copy()
 
-    def closest_distance_to_obstacle(self, point: np.ndarray) -> Union[float, np.ndarray]:
+    def closest_distance_to_obstacle(self, point: np.ndarray, radius: Union[float, np.ndarray] = 0.) -> \
+            Union[float, np.ndarray]:
         assert point.shape == (2,) or (len(point.shape) == 2 and point.shape[1] == 2)
         if point.shape == (2,):
-            distances = np.linalg.norm(point - self._circles[:, :2]).flatten()
-            min_idx = np.argmin(distances)
-            return distances[min_idx] - self._circles[min_idx, 2]
+            return np.min(np.linalg.norm(point - self._circles[:, :2]).flatten() - self._circles[:, 2] - radius)
         else:
             # TODO: Vectorization
-            return np.array([self.closest_distance_to_obstacle(p) for p in point])
+            return np.array([self.closest_distance_to_obstacle(p, r) for p, r in zip(point, radius)])
 
     @property
     def circles(self) -> np.ndarray:
