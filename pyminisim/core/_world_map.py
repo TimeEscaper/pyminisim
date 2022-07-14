Min obstacle dist calculation fixed

pyminisim/core/_world_map.py
@@ -7,7 +7,7 @@ import numpy as np
 class AbstractWorldMap(ABC):
 
     @abstractmethod
-    def closest_distance_to_obstacle(self, point: np.ndarray, radius: float = 0.) -> Union[float, np.ndarray]:
+    def closest_distance_to_obstacle(self, point: np.ndarray) -> Union[float, np.ndarray]:
         raise NotImplementedError()
 
     @abstractmethod

pyminisim/world_map/_circles_world.py
@@ -16,12 +16,12 @@ class CirclesWorld(AbstractWorldMap):
         super(CirclesWorld, self).__init__()
         self._circles = circles.copy()
 
-    def closest_distance_to_obstacle(self, point: np.ndarray, radius: Union[float, np.ndarray] = 0.) -> \
+    def closest_distance_to_obstacle(self, point: np.ndarray) -> \
             Union[float, np.ndarray]:
         assert point.shape == (2,) or (len(point.shape) == 2 and point.shape[1] == 2)
 
         if len(point.shape) == 1:
-            return np.linalg.norm(point - self._circles[:, :2], axis=1) - self._circles[:, 2]
+            return np.min(np.linalg.norm(point - self._circles[:, :2], axis=1) - self._circles[:, 2])
 
         pairwise_dist = cdist(point, self._circles[:, :2], metric="euclidean")
         pairwise_dist = pairwise_dist - self._circles[:, 2]

pyminisim/world_map/_empty_world.py
@@ -10,7 +10,7 @@ class EmptyWorld(AbstractWorldMap):
     def __init__(self):
         super(EmptyWorld, self).__init__()
 
-    def closest_distance_to_obstacle(self, point: np.ndarray, radius: float = 0.) -> Union[float, np.ndarray]:
+    def closest_distance_to_obstacle(self, point: np.ndarray) -> Union[float, np.ndarray]:
         assert point.shape == (2,) or (len(point.shape) == 2 and point.shape[1] == 2)
         if point.shape == (2,):
             return np.inf