Lines world closest point calculation fixed

examples/test.py
@@ -18,6 +18,10 @@ def create_sim() -> Tuple[pms.core.Simulation, pms.visual.Renderer]:
     world = pms.world_map.LinesWorld(lines=np.array([[[2., -2],
                                                       [2., 2]],
                                                      [[-2, -2],
+                                                      [-2, 2]],
+                                                     [[2., -2],
+                                                      [-2, -2]],
+                                                     [[2, 2],
                                                       [-2, 2]]]))
     # world = pms.world_map.LinesWorld(lines=np.array([[[2., 0.],
     #                                                   [2., 3.]],
@@ -29,7 +33,7 @@ def create_sim() -> Tuple[pms.core.Simulation, pms.visual.Renderer]:
                               world_map=world,
                               robot_model=robot_model,
                               pedestrians_model=None,
-                              sensors=[])
+                              sensors=sensors)
     renderer = pms.visual.Renderer(simulation=sim,
                                    resolution=80.0,
                                    screen_size=(500, 500))

pyminisim/world_map/_lines_world.py
@@ -42,7 +42,7 @@ class LinesWorld(AbstractStaticWorldMap):
         ab = lines[:, 1, :] - lines[:, 0, :]  # (n_lines, 2)
         ap = point - lines[:, np.newaxis, 0, :]  # (n_lines, n_points, 2)
         proj = np.einsum("ijk,ik->ij", ap, ab)  # (n_lines, n_points)
-        d = proj / (np.linalg.norm(ab) ** 2)  # (n_lines, n_points)
+        d = proj / (np.linalg.norm(ab, axis=1) ** 2)[:, np.newaxis]  # (n_lines, n_points)
         d = np.clip(d, 0., 1.)
         closest_points = lines[:, np.newaxis, 0, :] + \
                          ab[:, np.newaxis, :] * d[:, :, np.newaxis]  # (n_lines, n_points, 2)